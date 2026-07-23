704b5a1e401d08fdb88fb021928796d351718080
Scripthub <stepcurry505@gmail.com>
2026-07-24 07:31:15 +0800

    Update Waguri.lua

--- Waguri.lua
+++ Waguri.lua
@@ -1,2 +1,1 @@
-loadstring(game:HttpGet("https://cdn.sourceb.in/bins/GlOObznWac/0", true))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Waguriiiii/Murder-mystery-2/refs/heads/main/Waguri.lua"))()
+loadstring(game:HttpGet("https://cdn.sourceb.in/bins/jbH6babzL6/0"))()
